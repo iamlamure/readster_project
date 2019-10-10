98ca7b2cbca03ad5caeec50e4d7f5671951a3f72
iamlamure <panuwat.chaiw@msu.ac.th>
2019-10-10 20:17:45 +0700

    update addbok page

--- client/src/components/AddBook.vue
+++ client/src/components/AddBook.vue
@@ -1,59 +1,53 @@
 <template>
 <div class="container">
-        <div class="jumbotron mt-5">
-            <h1>เพิ่มหนังสือ</h1>
-        <form>
-            <div class="container">
-                <div>
-                    <input type="text" class="form-control" placeholder="ชื่อหนังสือ">
-                    <input type="text" class="form-control" placeholder="ผู้เขียน">
-                    <div class="form-group">         
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">รายละเอียด/เนื้อเรื่องย่อ</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
+    <div class="jumbotron mt-5">
+        <h1>เพิ่มหนังสือ</h1>
+            <form v-on:submit.prevent="addbook">
+                <div class="container">
+                    <div>
+                        <input type="text" v-model="book_name" class="form-control" placeholder="ชื่อหนังสือ">
+                        <input type="text" v-model="author_name" class="form-control" placeholder="ผู้เขียน">
+                        <div class="form-group">         
+                        </div>
+                        <div class="form-group">
+                            <label for="bookdetail">รายละเอียด/เนื้อเรื่องย่อ</label>
+                            <textarea v-model="book_detail" class="form-control" id="bookdetail" rows="3"></textarea>
+                        </div>
 
-                    <!-- Vue IF dropdown-->
-                    <div class="form-row">
-                        <div class="form-group col-md-2">
-                            <!-- Vue IF dropdown-->
-                            <select class="form-control" id="exampleFormControlSelect1">
-                            <option>SE-ED</option>
-                            <option>แสงดาว</option>
-                            <option>นายอินทร์</option>
-                            <option>สยามดรีม</option>
-                            <option>ป๊อป</option>
-                        </select>
-                        </div>
-                        <div class="form-group col-md-2">
-                            <!-- Vue IF dropdown-->
-                        <select class="form-control" id="exampleFormControlSelect2">
-                            <option>เลือกหมวดหมู่</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                        </div>
-                        <div class="form-group col-md-2">
-                            <input type="text" class="form-control" placeholder="ราคา">
-                        </div>
-                        <div class="form-group col-md-2">
-                            <input type="text" class="form-control" placeholder="จำนวนหน้า">
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                        <!-- Vue IF dropdown-->
+                        <div class="form-row">
+                            <div class="form-group col-md-2">
+                                <!-- Vue IF dropdown-->
+                                <select class="form-control" v-model="publisher" id="exampleFormControlSelect1">
+                                <option>SE-ED</option>
+                                <option>แสงดาว</option>
+                                <option>นายอินทร์</option>
+                                <option>สยามดรีม</option>
+                                <option>ป๊อป</option>
+                            </select>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <!-- Vue IF dropdown-->
+                            <select class="form-control" v-model="category" id="exampleFormControlSelect2">
+                                <option>เลือกหมวดหมู่</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <input type="number" v-model="price" class="form-control" placeholder="ราคา">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <input type="number" v-model="pages" class="form-control" placeholder="จำนวนหน้า">
+                            </div>
                         </div>
                     </div>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">บันทึก</button>
                 </div>
-                <button type="button" class="btn btn-primary btn-lg btn-block">บันทึก</button>
-            </div>
-        </form>
+            </form>
         </div>
     </div>
 </template>
@@ -64,7 +58,7 @@ import axios from 'axios'
 import router from '../router'
 
 export default {
-    
+
     data () {
         return {
                book_name:'',
@@ -74,13 +68,13 @@ export default {
                category: '',
                price: '',
                pages: '',
-               book_img: ''
+               //book_img: ''
         }
     },
 
     methods: {
-        bookadd() {
-            axios.post('/books/addbooks',
+        addbook() {
+            axios.post('/books/addbook',
                 {
                     book_name: this.book_name,
                     author_name: this.author_name,
@@ -89,10 +83,17 @@ export default {
                     category: this.category,
                     price: this.price,
                     pages: this.pages,
-                    book_img: this.book_img  
+                   // book_img: this.book_img  
                 }
-            )
+            ).then((res) => {
+                router.push({ name: 'Addbook'})
+                console.log(res)
+            }).catch((err) => {
+                console.log(err)
+            })
         }
     }
+
+
 }
 </script>
