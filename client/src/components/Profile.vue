bbe586841e69325879c8c94711666703a5db0e4e
iamlamure <panuwat.chaiw@msu.ac.th>
2019-10-30 21:45:30 +0700

    update 30/10/62 profile page

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,35 +1,49 @@
 <template>
-  <div class="container columns is-centered">
-    <div class="card">
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-          </figure>
+  <div>
+      
+      <h1 class="title columns is-mobile is-centered"> ข้อมูลผู้ใช้ </h1>
+      <section class="section columns is-half is-centered">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <figure class="image is-64x64">
+                <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+              </figure>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>{{first_name}} {{last_name}}</strong> <small>@{{id}}</small>
+                  <br>
+                  email : {{email}} 
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+              <nav class="level is-mobile">
+                <div class="level-left">
+                  <a class="level-item" aria-label="reply">
+                    <span class="icon is-small">
+                      <i class="fas fa-reply" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="retweet">
+                    <span class="icon is-small">
+                      <i class="fas fa-retweet" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="like">
+                    <span class="icon is-small">
+                      <i class="fas fa-heart" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </article>
         </div>
-        <div class="media-content">
-          <p class="title is-4">{{first_name}} {{last_name}}</p>
-          <p class="subtitle is-6">@{{id}} email : {{email}}</p>
-        </div>
-      </div>
-
-    <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-      <br>
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-    </div>
+      </section>
   </div>
-</div>
-  </div>
-
 </template>
 
 <script>
