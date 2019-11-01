37136cbcf274fe063a75a46e6bc51a637b760c9c
iamlamure <panuwat.chaiw@msu.ac.th>
2019-11-02 02:19:43 +0700

    update login and home page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,20 +1,68 @@
 <template>
-  <div class="container">
-    <div class="jumbotron mt-5">
-      <div class="col-sm-8 mx-auto">
-        <h1 class="text-center">WELCOME</h1>
-        <form>
-          <h1 class="title columns is-centered">ค้นพบ บทความ หรือ หนังสือที่ตรงตามใจคุณ</h1>
-          <input class="input" type="text" name="bookname" v-model="search">
-        </form>
-        <div>
-          <ul>
-            <li v-for="(book) in books" v-bind:key="book.bookid" v-bind:title="book.book_name" >{{book.book_name}}</li>
-          </ul>
+  <div class="container is-fluid">
+    <div class="container">
+      <h1 class="title  columns is-mobile is-centered"> ค้นพบบทความ และ หนังสือที่คุณถูกใจ </h1>
+      <br>
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half has-background-primary">
+          <p class="bd-notification is-primary">
+            <input class="input is-rounded" type="text" placeholder="Rounded input">
+          </p>
         </div>
       </div>
-    </div>
-    <div>
+      <hr class="style11">
+      <br>
+      <h4 class="title is-4"> หนังสือใหม่ </h4>
+      <div class="container">
+          <div class="columns is-multiline">
+            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                <div class="tile is-child box">
+                    <div class="card-image has-text-centered">
+                        <figure class="image is-inline-block">
+                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
+                        </figure>
+                    </div>
+                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
+                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
+                </div>
+            </div>
+        </div>
+      </div>
+      <br>
+      <h4 class="title is-4"> วรรณกรรมใหม่ </h4>
+      <div class="container">
+          <div class="columns is-multiline">
+            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                <div class="tile is-child box">
+                    <div class="card-image has-text-centered">
+                        <figure class="image is-inline-block">
+                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
+                        </figure>
+                    </div>
+                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
+                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
+                </div>
+            </div>
+        </div>
+      </div>
+      <hr class="style11">
+      <br>
+      <h4 class="title is-4"> รีวิวใหม่ </h4>
+      <div class="container">
+          <div class="columns is-multiline">
+            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                <div class="tile is-child box">
+                    <div class="card-image has-text-centered">
+                        <figure class="image is-inline-block">
+                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
+                        </figure>
+                    </div>
+                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
+                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
+                </div>
+            </div>
+        </div>
+      </div>     
     </div>
   </div>
 </template>
--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-md-6 mt-5 mx-auto">
-        <form v-on:submit.prevent="login">
-          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-          <div class="form-group">
-            <label for="email">Email address</label>
-            <input type="email" v-model="email" class="form-control" name="email" placeholder="Enter email">
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" v-model="password" class="form-control" name="password" placeholder="Password">
-          </div>
-          <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
-        </form>
+    <div>
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+            <form v-on:submit.prevent="login">
+            <h1 class="title columns is-mobile is-centered">Please sign in</h1>
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input type="email" v-model="email" class="input" name="email" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" v-model="password" class="input" name="password" placeholder="Password">
+            </div>
+            <br>
+            <a type="submit" class="button is-primary columns is-centered">Sign in</a>
+          </form>
+        </div>
       </div>
     </div>
   </div>
