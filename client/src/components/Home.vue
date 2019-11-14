3aab0069189178879207f4aff525949c5e234413
iamlamure <panuwat.chaiw@msu.ac.th>
2019-11-14 23:50:05 +0700

    update mockup for capture 14/11/62

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -16,71 +16,71 @@
       <div class="container">
           <div class="columns is-multiline">
             <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/9/7/9786163016942.jpg">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">โฮโมดีอุส</h5>
+                                <h6  class="title columns is-mobile is-centered is-6 color-is">฿442</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/s/c/scan_6_10_1.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">Becoming</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿375</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">เลส</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿238</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_cat.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">อันตัวข้าพเจ้านี้คือแมว</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿476</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/9/7/9786167144726.jpg">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">เสรีนิยมยืนขึ้น</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿265</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/s/c/screen_shot_2562-11-06_at_1.38.01_pm.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">พ่อของผมคือผู้ก่อการร้าย</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿198</h6>
+                            </div>
+                        </div>
         </div>
       </div>
       <br>
@@ -88,71 +88,71 @@
       <div class="container">
           <div class="columns is-multiline">
             <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
-            <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
-                <div class="tile is-child box">
-                    <div class="card-image has-text-centered">
-                        <figure class="image is-inline-block">
-                            <img src="https://readery.co/media/catalog/product/cache/1/small_image/240x/17f82f742ffe127f42dca9de82fb58b1/b/o/book_l_less.png">
-                        </figure>
-                    </div>
-                    <h5  class="title columns is-mobile is-centered card-header-title is-5">ชื่อหนังสือ</h5>
-                    <h6  class="title columns is-mobile is-centered is-6">฿ราคา</h6>
-                </div>
-            </div>
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/s/c/screen_shot_2562-11-12_at_4.52.00_pm.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">ให้มันเป็นความลับต่อไป</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿232</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/s/c/screen_shot_2562-10-31_at_2.39.36_pm.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">Revenge: Eleven Dark Tales</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿252</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/s/c/scan_6_17.png">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">รหัสจากอเวจี</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿211</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/9/7/9786164480254.jpg">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">After School</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿251</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/9/7/9786164480247.jpg">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">นิ้วสีแดง</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿233</h6>
+                            </div>
+                        </div>
+                        <div class="column tile is-2 is-vertical is-mobile is-parent is-narrow">
+                            <div class="tile is-child box">
+                                <div class="card-image has-text-centered">
+                                    <figure class="image is-inline-block">
+                                        <img src="https://readery.co/media/catalog/product/cache/1/small_image/360x/17f82f742ffe127f42dca9de82fb58b1/f/i/file_95_21.jpg">
+                                    </figure>
+                                </div>
+                                <h5  class="title columns is-mobile is-centered card-header-title is-5">ตายวันแต่ง</h5>
+                                <h6  class="title columns is-mobile is-centered is-6">฿296</h6>
+                            </div>
+                        </div>
         </div>
       </div>
       <br>
